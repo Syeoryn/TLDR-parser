5df067a83f78b271e3005b401079c4ff12e9e882
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-26 21:57:25 -0700

    Fixes DOM traversal

--- tldr.js
+++ tldr.js
@@ -10,14 +10,16 @@ var findTextInDOM = function(node){
   node = node || document.body;
   
   for(var i = 0; i < node.children.length; i++){
-    if(node.children.length){
+    if(node.children[i].length){
       findTextInDOM(node.children[i]);
-    }
-    tldrs = parser(node.children[i].innerHTML);
-    if(tldrs){
-      for(var j = 0; j < tldrs.length; j++){
-        list.push(tldrs[j]);
+    } else {
+      tldrs = parser(node.children[i].innerHTML);
+      if(tldrs){
+        for(var j = 0; j < tldrs.length; j++){
+          list.push(tldrs[j]);
+        }
       }
+      
     }
   }
 }
