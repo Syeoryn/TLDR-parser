a251ffb0c06d87607abf2c7f049bc6acd7e02909
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-29 22:54:56 -0700

    Creates DOM element for message instead of alert message

--- tldr.js
+++ tldr.js
@@ -1,30 +1,35 @@
-var parser = function(text){
-  text = text.replace(/<.+>/g, '');
-  var tldr = text.toLowerCase().match(/tl\x3bdr[^\n]+|tl\x3b dr[^\n]+|tldr[^\n]+|tl dr[^\n]+/g);
-  return tldr;
-}
-
-var findTextOnPage = function(node){
-  node = node || document.body;
+var findTldrs = function(){
+  node = document.body;
   var list = [];
-   
-  tldrs = parser(document.body.innerText);
-  if(tldrs){
-    for(var j = 0; j < tldrs.length; j++){
-      list.push(tldrs[j]);
-    }
-  }
-  return list;
+  var text = node.innerText;
+  tldrs = text.toLowerCase().match(/tl\x3bdr[^\n]+|tl\x3b dr[^\n]+|tldr[^\n]+|tl dr[^\n]+/g)
+  return tldrs;
 }
 
 var findAll = function(){
-  var node = document.createElement('div');
-  node.className = 'tldr';
   var list =findTextOnPage();
   var message = '';
   for(var i = 0; i < list.length; i++){
     message += list[i] + '\n\n'
   }
+  if(!message) message = "No tl;drs!";
+
+  return message;
+}
+
+var makePopup = function(){
+  var node = document.createElement('div');
+  node.className = 'tldr';
+  node.innerText = message;
+  node.style.width = '300px';
+  node.style.position = 'fixed';
+  node.style.height = '500px';
+  node.style.top = '0';
+  node.style.right = '0';
+  node.style['background-color'] = 'red';
+  node.style.overflow = 'hidden';
+  node.style['z-index'] = 9001;
+  document.body.appendChild(node);
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
