a2bddf3c7e380c918ceaf79bd1b1b37cc3824e25
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-30 09:56:00 -0700

    Refactors code

--- tldr.js
+++ tldr.js
@@ -22,25 +22,25 @@ var createMessage = function(){
 }
 
 var makePopup = function(message){
-    var node = document.createElement('div');
-    node.className = 'tldr';
-    node.innerText = message;
-    node.style.width = '300px';
-    node.style.position = 'fixed';
-    node.style.top = '0';
-    node.style.right = '0';
-    node.style['background-color'] = '#AAAAAA';
-    node.style.overflow = 'hidden';
-    node.style['z-index'] = 9001;
-    node.style.opacity = 0.9;
-    node.style['font-size'] = '12px'
+  var node = document.createElement('div');
+  node.className = 'tldr';
+  node.innerText = message;
+  node.style['width'] = '300px';
+  node.style['position'] = 'fixed';
+  node.style['top'] = '0';
+  node.style['right'] = '0';
+  node.style['background-color'] = '#AAAAAA';
+  node.style['overflow'] = 'hidden';
+  node.style['z-index'] = 9001;
+  node.style['opacity'] = 0.9;
+  node.style['font-size'] = '12px'
   return node;
 }
 
 var removePopup = function(node, step){
   step = step || .001;
-  node.style.opacity -= step;
-  if(node.style.opacity <= .005){
+  node.style['opacity'] -= step;
+  if(node.style['opacity'] <= .005){
     document.body.removeChild(node);
     poppedUp = false;
   } else {
@@ -51,20 +51,19 @@ var removePopup = function(node, step){
 }
 
 var popup = function(showIfNoTldrs){
-  if(!poppedUp){
-    var message = createMessage();
-    var popup = makePopup(message);
-    var time = 5000;
-    var step = .001;
-    if(message === 'No tl;drs!'){
-      if(!showIfNoTldrs) return;
-      time = 1000;
-      step = .01;
-    }
-    document.body.appendChild(popup);
-    poppedUp = true;
-    setTimeout(function(){removePopup(popup, step)}, time);
+  if(poppedUp) return;
+  var message = createMessage();
+  var popup = makePopup(message);
+  var time = 5000;
+  var step = .001;
+  if(message === 'No tl;drs!'){
+    if(!showIfNoTldrs) return;
+    time = 1000;
+    step = .01;
   }
+  document.body.appendChild(popup);
+  poppedUp = true;
+  setTimeout(function(){removePopup(popup, step)}, time);
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
